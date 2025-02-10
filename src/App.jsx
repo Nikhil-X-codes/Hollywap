f4d183036d84c02341936856bc3bea2447d984e5
NIKHIL NAGAR <nagarn2005@gmail.com>
2025-02-10 09:45:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MovieCard from "./Component";
 import Home from "./Home";
-import Navbar from "./Navbar";
+import Navbar from "./navbar";
 import Fav from "./Fav";
 import "./index.css";
 import { FavoriteProvider } from "./FavoriteContext";
